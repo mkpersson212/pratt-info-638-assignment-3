893c3355b00f0a495ca7578f8b9aa98dbfe0de8f
Michael Persson <mpersson@pratt.edu>
2026-04-11 07:40:38 -0400

    Added check for logged in status, correct user editing comments, and removed broken router

--- routes/comments.js
+++ routes/comments.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
-const BookUser = require('../models/book_user');
-const bookComment = require('../models/comment');
-
-
-router.post('/upsert', async (req, res) => {
-  const { bookId, text } = JSON.stringify(req.body); //this 
-  await Comment.create({ bookId, userEmail: req.user._id, text });
-  res.redirect(`/books/${bookId}`);
-});
-
+const bookComment = require('../models/comment'); 
 
 router.post('/upsert', async (req, res, next) => {
-  console.log('body: ' + JSON.stringify(req.body))
+  const {bookId} = req.body;
+  the below if statement checks to see if the user is logged in
+  if (!req.session.currentUser){
+    req.session.flash = {
+      type: 'danger',
+      intro: 'Please log in.',
+      message: 'You can\'t comment until you\'ve logged in to BookedIn!'
+    }
+    return res.redirect(303, '/users/login');
+  }
+  req.body.userEmail = req.session.currentUser.email;
   bookComment.upsert(req.body);
   let createdOrupdated = req.body.id ? 'updated' : 'created';
   req.session.flash = {
@@ -21,7 +22,31 @@ router.post('/upsert', async (req, res, next) => {
     intro: 'Success!',
     message: `Your comment has been ${createdOrupdated}!`,
   };
-  res.redirect(303, '/books/$book.Id');
+  res.redirect(303, `/books/show/${bookId}`);
+});
+
+router.get('/edit', async (req, res, next) => {
+  let commentIndex = req.query.id;
+  let comment = bookComment.get(commentIndex);
+//the below if statements check to see if the user is logged in
+//and if they're the correct user to be editing the comment
+  if (!req.session.currentUser){
+    req.session.flash = {
+      type: 'danger',
+      intro: 'Please log in.',
+      message: 'You can\'t comment until you\'ve logged in to BookedIn!'
+    }
+    return res.redirect(303, '/users/login');
+  }  
+  if (req.session.currentUser.email !== comment.userEmail){
+    req.session.flash = {
+      type: 'danger',
+      intro: 'Whoops',
+      message: 'You\'re unable to edit other users\' comments.'
+    }
+    return res.redirect(303, '/books');
+  }
+  res.render('comments/form', { title: 'BookedIn || Edit Comment', comment, commentIndex});
 });
 
 module.exports = router;
